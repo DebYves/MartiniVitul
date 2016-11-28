74ba85826d04d32abf36ff5404e8e28ca59dd793
mrouffiange <etu26949@student.henallux.be>
2016-11-28 22:22:49 +0100

    --

--- test/test/MainPage.xaml.cs
+++ test/test/MainPage.xaml.cs
@@ -14,7 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-//gdggddgdgdgdgdg
+//epic
 namespace test
 {
     /// <summary>
