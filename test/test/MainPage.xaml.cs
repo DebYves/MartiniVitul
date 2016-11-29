7dbdd3a8866f9399f833362fe1eefbf7e286e5f8
Debast Yves <debyves@hotmail.com>
2016-11-29 10:19:03 +0100

    hahaha

--- test/test/MainPage.xaml.cs
+++ test/test/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+//Cher Martin, si tu réouvres ce projo et l'update un jour, je te paye un coca héhé. Guez
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 //epic
 namespace test
